d0dc31763666bc60a0d4f32d29f82f78cfa04932
DaLegendaryCoder13 <86676894+DaLegendaryCoder13@users.noreply.github.com>
2021-07-07 16:25:48 -0500

    Add files via upload

---
+++ Calculator .playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Input Variables
+var Num1 = 182
+var Num2 = 69
+
+// The Operation
+var add = Num1 + Num2
+var sub = Num1 - Num2
+var mul = Num1 * Num2
+var div = Num1 / Num2
+
+// Print
+
+print (add)
+print (sub)
+print (mul)
+print (div)
